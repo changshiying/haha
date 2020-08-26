7497630dcd20b5dc9e4ca281b9dc8210f9919754
cangshiying <1301830746@qq.com>
2020-08-26 13:45:09 +0800

    2232

---
+++ lesson_2.py
@@ -0,0 +1,214 @@
+# a =12
+# # print(type(a))
+# # print(isinstance(a,str
+# #                  ))
+
+
+# b=1.326
+# print(type(b))
+
+# c=False
+# print(type(c))
+# print(isinstance(c,str))
+
+# a="suchenbanzuilihai!" \
+# # #   "wohaopang"
+# # # print(a)
+# # # print(type(a))
+
+# a='asjhdsjad' \
+#   'sajhd'
+# print(a)
+
+# a='''asjhdsajhd
+# # ksadksajd
+# # a'''
+# # print(a)
+#
+# a='abcd或许真的不爱了吧!!!'
+# # print(a[0:5])
+# # # print(a[0:6:2])
+# # # print(a[::-1])
+# # # print(a[-1:-5:-1])
+# # print(a[-1:-5:-1])
+# # print(len(a))
+#
+# str=a.replace("abcd","laugh")
+# print(str)
+# str=a.replace("或许","")
+# print(str)
+#
+# a='kaoren,zhinengkaoziji!'
+# print(a.index('ziji'))
+# print(a.find('ziji'))
+
+# 格式化输出
+# name='jiao'
+# age=24
+# xingbie='女'
+# print('''
+# -----------教师信息--------
+# name:{0}
+# xingbie:{2}
+# age:{1}
+# '''.format(name,age,xingbie))
+# a='cangshiying\n'
+# # b='liujiao'
+# # print(a*10)
+#
+# a=3
+# b=9
+# print(a==b)
+# print(10<7 or  6>7)
+# a='woshihaoren'
+# print('wo' in a)
+
+# 姓名=input('请输入姓名：')
+# 年龄=input('请输入年龄：')
+# 性别=input('请输入性别：')
+# print('''
+# **********************
+# 姓名:{0}
+# 性别:{2}
+# 年龄:{1}
+# **********************
+# '''.format(姓名,年龄,性别))
+
+
+# 姓名=input('请输入姓名：')
+# 年龄=input('请输入年龄：')
+# 性别=input('请输入性别：')
+# print('''
+# **********************
+# 姓名:{}
+# 性别:{}
+# 年龄:{}
+# **********************
+# '''.format(姓名,性别,年龄))
+
+str='python hello aaa 123123aabb'
+# print(str[0:6])  第一问
+# print(str.find('o a'))第二问
+# print(str.find('he'))
+# print(str.find('ab'))
+# str=str.replace("pthon","lemon")第三问
+# print(str)
+# print(str.find('aaa'))第四问
+
+# list1=[1,2,'sajhfh',56,1,[1,2,3]]
+# print(list1)
+# print(type(list1))
+# print(list1[1:5])
+# print(list1[5][1])
+
+# list1.append('刘娇')
+# print(list1)
+# list1.append(['liu','cang'])
+# print(list1)
+
+# list1.extend(['liu','cang','yiyi'])
+# print(list1)
+# list1.extend('liu')
+# print(list1)
+#
+# list1.insert(3,'liu')
+# print(list1)
+
+# list1=[1,2,'sajhfh',56,1,[1,2,3],'花花']
+# list1.pop()
+# print(list1)
+# list1.pop(2)
+# print(list1)
+
+# list1.remove(56)
+# print(list1)
+#
+# list1.clear()
+# print(list1)
+#
+# list1[4]='jiaojiao'
+# print(list1)
+#
+# len(list1)
+# print(len(list1))
+# print(list1.count(2))
+
+
+
+# tuple1=()
+# print(type(tuple1))
+
+#
+# tuple1=(1,2,2,3,'yiyi',[1,5,2,3])
+# # print(tuple1[5][2])
+#
+# list1=list(tuple1)
+# print(list(tuple1))
+# list1[3]='na'
+# print(list1)
+# tuple1=tuple(list1)
+# print(tuple1)
+
+# dict1=()
+# print(type(dict1))
+
+# dict1={"name":"huahua","age":"18","xingbie":"nv","hobby":"足球，篮球，乒乓球，玩手机"}
+# print(dict1)
+#
+# print(dict1['hobby'])
+# print(dict1.get('age'))
+
+
+# dict1.update({"city":"chansha","shouji":"110"})
+# print(dict1)
+#
+# dict1['tizhong']=175
+# print(dict1)
+#
+# dict1['tizhong']=185
+# print(dict1)
+#
+
+# dict1.pop('age')
+# print(dict1)
+
+
+
+# money= int  (input('请输入你的工资：'))
+# if money >= 15000:
+#     print('我要去旅游')
+# elif money >= 10000:
+#         print('我去吃火锅')
+# else:
+#     print("banzhuan")
+
+# str='scb是最棒的！'
+# for item in str:
+#     if item =='b' :
+#         continue
+#     print(item)
+
+
+# str='scb是最棒的！'
+# for item in str:
+#     if item=='c':
+#         break
+#     print(item)
+
+
+# a=[1,2,'6','summer']第一题
+# print('i' in a)
+
+# dict_1={"class_id":45,"num":20}
+# num=int(input('请输入人数：'))
+# if num>5:
+#     print(num)
+# else:
+#     print()
+
+
+# list3=['囧囧','旧模样','ouyang','Nacy','土豆江']
+# list3[0]='"姓名":"囧囧"'
+# list3[j]
+
+
